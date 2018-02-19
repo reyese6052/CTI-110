debaed78bac6df8b045655ca17e45b19ad826075
reyese6052 <36610807+reyese6052@users.noreply.github.com>
2018-02-18 19:04:19 -0500

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_ReyesEvita.py
@@ -0,0 +1,10 @@
+#CTI-110
+#P2T1-Sales Prediction
+#Evita Reyes
+#February 14, 2018
+#
+
+total_sales = float(input('Enter the projected amount of total sales: '))
+profit = total_sales * 0.23
+print ('The annual profit is $', format(profit,',.2f'))
+
