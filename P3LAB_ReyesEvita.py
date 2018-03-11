fee33a26008c971ba889badfc41e6d69d4fb2e80
Evita <36610807+reyese6052@users.noreply.github.com>
2018-03-11 19:51:41 -0400

    Add files via upload
    P3LAB

---
+++ P3LAB_ReyesEvita.py
@@ -0,0 +1,30 @@
+# CTI-110
+# P3Lab
+# Evita Reyes
+# March 11, 2018
+#
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+    score = int(input('Enter grade: '))
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    F_score = 50
+
+
+    if score >= A_score:
+        print('Your grade is: A')
+    elif score >= B_score:
+        print('Your grade is: B')
+    elif score >= C_score:
+        print('Your grade is: C')
+    elif score >= D_score:
+        print('Your grade is: D')
+    else:
+        print('Your grade is: F')
+    
+# program start
+main()
