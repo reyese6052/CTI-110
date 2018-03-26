098e214ca207bdf94555b796bac5f56a4525396f
Evita <36610807+reyese6052@users.noreply.github.com>
2018-03-25 23:14:06 -0400

    Add files via upload

---
+++ M4LAB2_ReyesEvita.py
@@ -0,0 +1,40 @@
+# CTI-110
+# P4LAB2:Initials
+# Evita L. Reyes
+# March 25, 2018
+#
+import turtle
+win = turtle.Screen()
+t = turtle.Turtle()
+t.pensize(5)
+t.color("orange")
+t.shape("turtle")
+
+t.backward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.backward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+t.penup()
+t.forward(50)
+t.pendown()
+
+t.left(90)
+t.forward(200)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.backward(50)
+t.left(130)
+t.forward(100)
+
+win.mainloop()
