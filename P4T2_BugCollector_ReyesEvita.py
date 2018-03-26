69988c4ad88b16f9ee369de71374791529614685
Evita <36610807+reyese6052@users.noreply.github.com>
2018-03-25 23:33:21 -0400

    Add files via upload

---
+++ P4T2_BugCollector_ReyesEvita.py
@@ -0,0 +1,12 @@
+# CTI-110
+# P4T2:Bug Collector
+# Evita Reyes
+# March 25, 2018
+#
+total=0
+for day in range(1, 8):
+    print('Enter the bugs collected on day', day)
+    bugs = int(input())
+    total = total+bugs
+
+print('You collected a total of', total, 'bugs.')
