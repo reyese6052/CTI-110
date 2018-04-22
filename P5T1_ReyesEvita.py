e471f118ee72cbbb5d7ca08e50c6937bd2cca87c
Evita <36610807+reyese6052@users.noreply.github.com>
2018-04-22 00:35:10 -0400

    Add files via upload

---
+++ P5T1_ReyesEvita.py
@@ -0,0 +1,16 @@
+# Convert kilometer to miles
+# April 21, 20158
+# CTI-110 P5T1_KilometerConverter
+# Evita Reyes
+#
+CONVERSION_FACTOR = 0.6214
+
+def main():
+    kilometers=float(input('Enter a distance in kilometers.'))
+    show_miles(kilometers)
+
+def show_miles(km):
+    miles = km * CONVERSION_FACTOR
+    print (km, 'kilometers equals', miles, 'miles.')
+
+main()
